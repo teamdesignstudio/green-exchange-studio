5990b6dd64c28a413211352be0c725d5f0273f53
Ioan Butiu <ioan.butiu@gmail.com>
2026-03-04 19:42:27 +0100

    sync 8d141eb

--- schemas/index.js
+++ schemas/index.js
@@ -1,70 +1,72 @@
 // Document schemas
-import siteSettings from "./documents/siteSettings";
-import home from "./documents/home";
-import about from "./documents/about";
-import team from "./documents/team";
-import news from "./documents/news";
-import faq from "./documents/faq";
-import contact from "./documents/contact";
-import privacyPolicy from "./documents/privacyPolicy";
-import givingBack from "./documents/givingBack";
-import page from "./documents/page";
-import category from "./documents/category";
-import article from "./documents/article";
-import author from "./documents/author";
-import careers from "./documents/careers";
-import listing from "./documents/listing";
-import trading from "./documents/trading";
-import regulatory from "./documents/regulatory";
+import siteSettings from './documents/siteSettings';
+import home from './documents/home';
+import about from './documents/about';
+import team from './documents/team';
+import news from './documents/news';
+import faq from './documents/faq';
+import contact from './documents/contact';
+import privacyPolicy from './documents/privacyPolicy';
+import givingBack from './documents/givingBack';
+import page from './documents/page';
+import category from './documents/category';
+import article from './documents/article';
+import author from './documents/author';
+import careers from './documents/careers';
+import listing from './documents/listing';
+import trading from './documents/trading';
+import regulatory from './documents/regulatory';
 
 // Object schemas
-import blockContent from "./objects/blockContent";
-import hero from "./objects/hero";
-import imageSection from "./objects/imageSection";
-import imageWide from "./objects/imageWide";
-import faqItem from "./objects/faqItem";
-import teamMember from "./objects/teamMember";
-import teamGroup from "./objects/team";
-import textGridItem from "./objects/textGridItem";
-import textSection from "./objects/textSection";
-import cta from "./objects/cta";
-import iconRowItem from "./objects/iconRowItem";
-import podcastPlayer from "./objects/podcastPlayer";
-import regulationItem from "./objects/documentItem";
-import regulationModule from "./objects/documentModule";
+import blockContent from './objects/blockContent';
+import hero from './objects/hero';
+import imageSection from './objects/imageSection';
+import imageWide from './objects/imageWide';
+import faqItem from './objects/faqItem';
+import teamMember from './objects/teamMember';
+import teamGroup from './objects/team';
+import textGridItem from './objects/textGridItem';
+import textSection from './objects/textSection';
+import cta from './objects/cta';
+import iconRowItem from './objects/iconRowItem';
+import podcastPlayer from './objects/podcastPlayer';
+import regulationModule from './objects/regulationModule';
+import documentCategory from './objects/documentCategory';
+import regulationItem from './objects/documentItem';
 
 export const schemaTypes = [
-  // Document types
-  siteSettings,
-  home,
-  about,
-  team,
-  news,
-  faq,
-  contact,
-  article,
-  author,
-  category,
-  privacyPolicy,
-  givingBack,
-  careers,
-  page,
-  listing,
-  trading,
-  regulatory,
-  // Object types
-  hero,
-  imageSection,
-  imageWide,
-  blockContent,
-  faqItem,
-  teamMember,
-  teamGroup,
-  textGridItem,
-  textSection,
-  cta,
-  iconRowItem,
-  podcastPlayer,
-  regulationItem,
-  regulationModule,
+	// Document types
+	siteSettings,
+	home,
+	about,
+	team,
+	news,
+	faq,
+	contact,
+	article,
+	author,
+	category,
+	privacyPolicy,
+	givingBack,
+	careers,
+	page,
+	listing,
+	trading,
+	regulatory,
+	// Object types
+	hero,
+	imageSection,
+	imageWide,
+	blockContent,
+	faqItem,
+	teamMember,
+	teamGroup,
+	textGridItem,
+	textSection,
+	cta,
+	iconRowItem,
+	podcastPlayer,
+	regulationItem,
+	regulationModule,
+	documentCategory,
 ];
--- structure.js
+++ structure.js
@@ -1,92 +1,71 @@
-import { structureTool } from "sanity/structure";
+import { structureTool } from 'sanity/structure';
 
 // We filter document types defined in structure to prevent
 // them from being listed twice
 const hiddenDocTypes = (listItem) =>
-  ![
-    "page",
-    "home",
-    "about",
-    "team",
-    "news",
-    "faq",
-    "contact",
-    "siteSettings",
-    "article",
-    "author",
-    "category",
-    "privacyPolicy",
-    "givingBack",
-    "careers",
-    "listing",
-    "trading",
-    "regulatory",
-  ].includes(listItem.getId());
+	![
+		'page',
+		'home',
+		'about',
+		'team',
+		'news',
+		'faq',
+		'contact',
+		'siteSettings',
+		'article',
+		'author',
+		'category',
+		'privacyPolicy',
+		'givingBack',
+		'careers',
+		'listing',
+		'trading',
+		'regulatory',
+	].includes(listItem.getId());
 
 export const structure = structureTool({
-  name: "content",
-  title: "Content",
-  structure: (S) =>
-    S.list()
-      .title("Content")
-      .items([
-        S.documentListItem().id("home").schemaType("home").title("Home"),
-        S.documentListItem()
-          .id("givingBack")
-          .schemaType("givingBack")
-          .title("Vision"),
-        S.documentListItem()
-          .id("listing")
-          .schemaType("listing")
-          .title("Listing"),
-        S.documentListItem()
-          .id("trading")
-          .schemaType("trading")
-          .title("Trading"),
-        S.documentListItem()
-          .id("regulatory")
-          .schemaType("regulatory")
-          .title("Regulatory"),
-        S.documentListItem().id("team").schemaType("team").title("Team"),
-        S.documentListItem().id("news").schemaType("news").title("News"),
-        S.documentListItem()
-          .id("careers")
-          .schemaType("careers")
-          .title("Careers"),
-        S.documentListItem().id("about").schemaType("about").title("About"),
-        S.documentListItem()
-          .id("contact")
-          .schemaType("contact")
-          .title("Contact"),
-        S.documentListItem().id("faq").schemaType("faq").title("FAQ"),
-        S.documentListItem()
-          .id("privacyPolicy")
-          .schemaType("privacyPolicy")
-          .title("Privacy Policy"),
-        S.divider(),
-        S.listItem()
-          .title("News Content")
-          .child(
-            S.list()
-              .title("News")
-              .items([
-                S.listItem()
-                  .title("Articles")
-                  .child(S.documentTypeList("article").title("Articles")),
-                S.listItem()
-                  .title("Authors")
-                  .child(S.documentTypeList("author").title("Authors")),
-                S.listItem()
-                  .title("Categories")
-                  .child(S.documentTypeList("category").title("Categories")),
-              ])
-          ),
+	name: 'content',
+	title: 'Content',
+	structure: (S) =>
+		S.list()
+			.title('Content')
+			.items([
+				S.documentListItem().id('home').schemaType('home').title('Home'),
+				S.documentListItem().id('about').schemaType('about').title('Vision'),
+				S.documentListItem().id('team').schemaType('team').title('Team'),
+				S.documentListItem().id('news').schemaType('news').title('News'),
+				S.documentListItem().id('careers').schemaType('careers').title('Careers'),
+				S.documentListItem().id('givingBack').schemaType('givingBack').title('About'),
+				S.documentListItem().id('faq').schemaType('faq').title('FAQ'),
+				S.documentListItem().id('contact').schemaType('contact').title('Contact'),
+				S.documentListItem().id('privacyPolicy').schemaType('privacyPolicy').title('Privacy Policy'),
+				S.divider(),
+				S.listItem()
+					.title('Regulation')
+					.child(
+						S.list()
+							.title('Regulation')
+							.items([
+								S.documentListItem().id('listing').schemaType('listing').title('Listing'),
+								S.documentListItem().id('trading').schemaType('trading').title('Trading'),
+								S.documentListItem().id('regulatory').schemaType('regulatory').title('Regulatory'),
+							]),
+					),
+				S.divider(),
+				S.listItem()
+					.title('News Content')
+					.child(
+						S.list()
+							.title('News')
+							.items([
+								S.listItem().title('Articles').child(S.documentTypeList('article').title('Articles')),
+								S.listItem().title('Authors').child(S.documentTypeList('author').title('Authors')),
+								S.listItem().title('Categories').child(S.documentTypeList('category').title('Categories')),
+							]),
+					),
 
-        ...S.documentTypeListItems().filter(hiddenDocTypes),
-        S.divider(),
-        S.documentListItem()
-          .id("siteSettings")
-          .schemaType("siteSettings")
-          .title("Site Settings"),
-      ]),
+				...S.documentTypeListItems().filter(hiddenDocTypes),
+				S.divider(),
+				S.documentListItem().id('siteSettings').schemaType('siteSettings').title('Site Settings'),
+			]),
 });
